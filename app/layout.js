00f0387032d7926ec29440fd23442ff4fe0bc1f4
haruk <haruki.u@gmail.com>
2025-12-12 12:55:53 +0900

    ファビコンを修正

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "GenAI App",
   description: "Multimodal GenAI Application",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({ children }) {
