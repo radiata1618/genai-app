d00cd2461bcddbadb6c04b2cbf0bdfccbe692742
haruk <haruki.u@gmail.com>
2026-01-01 09:15:23 +0900

    ビルドエラーを修正

--- app/recipes/page.js
+++ app/recipes/page.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { getRecipes } from '../actions/recipe';
 import StarRating from '../../components/StarRating';
+import MobileMenuButton from '../../components/MobileMenuButton';
 
 export default function RecipeListPage() {
     const [recipes, setRecipes] = useState([]);
